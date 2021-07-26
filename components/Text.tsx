5cab13767ffd413433667ae443f0b70af4617121
Travis Byrman <travis-byrman@live.nmit.ac.nz>
2021-07-26 17:11:51 +1200

    Update Text.tsx

--- components/Text.tsx
+++ components/Text.tsx
@@ -1,66 +1,49 @@
-import React from 'react';
+import React from "react";
 
-interface ITextParameters {
-  children: React.ReactNode | string,
-  weight: string,
-  family: string,
-  color: string,
-  size: string,
-}
-
-const sizes: object = {
+const sizes = {
   default: 14,
   small: 12,
-  title: 18,
 };
 
-const colors: object = {
-  default: 'black',
-  'grey-lighter': '#999999',
-  'gray-light': '#e1e4e8',
-  gray: '#586069',
-  'gray-dark': '#24292e',
-  'standard': 'rgba(115, 115, 115, .8)',
+const colors = {
+  default: "#24292e",
+  "gray-light": "#e1e4e8",
+  gray: "#586069",
+  "gray-dark": "#24292e",
 };
 
-const families: object = {
-  default: '-apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji',
-  mono: 'SFMono-Regular,Consolas,Liberation Mono,Menlo,monospace',
+const families = {
+  default:
+    "-apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji",
+  mono: "SFMono-Regular,Consolas,Liberation Mono,Menlo,monospace",
 };
 
-const weights: object = {
+const weights = {
   default: 400,
   bold: 600,
 };
 
-/**
- * Text
- * Simple text line with styles as props.
- * @param weight
- * @param family 
- * @param color
- * @param size 
- */
 const Text: React.FC<any> = ({
-  children = '',
-  weight = 'default',
-  family = 'default',
-  color = 'default',
-  size = 'default',
+  children = "",
+  weight = "default",
+  family = "default",
+  color = "default",
+  size = "default",
   ...props
-}: ITextParameters) => {
+}) => {
   return (
     <p
       style={{
-        color: colors[color],
-        fontFamily: families[family],
+        whiteSpace: "pre",
         fontSize: `${sizes[size]}px`,
-        fontWeight: weights[weight],
         lineHeight: 1.5,
-        whiteSpace: 'pre',
+        fontFamily: families[family],
+        color: colors[color],
+        fontWeight: weights[weight],
       }}
-      { ...props }>
-      { children }
+      {...props}
+    >
+      {children}
     </p>
   );
 };
